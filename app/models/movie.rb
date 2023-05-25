2aed47174936e0a6b52216b69c46fec65fd19d2a
kantonio88 <keston.antonio.schmidtgen@gmail.com>
2023-05-25 17:37:16 +0200

    The challenege is not complete without errors. All green

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,18 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+  has_many :list, through: :bookmarks
   validates :title, presence: true, uniqueness: true
-  validates :overview, presence: true, uniqueness: true
-
-  has_many :bookmarks, dependent: :destroy
-  has_many :lists, through: :bookmarks
-
-  before_destroy :ensure_not_referenced_by_any_bookmark
-
-  private
-
-  def ensure_not_referenced_by_any_bookmark
-    unless bookmarks.empty?
-      errors.add(:base, 'Cannot delete movie with associated bookmarks')
-      throw :abort
-    end
-  end
+  validates :overview, presence: true
 end

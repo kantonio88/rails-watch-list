9034280baa7e0b3292da7ef0b6b415b4166de7a4
kantonio88 <keston.antonio.schmidtgen@gmail.com>
2023-05-25 16:58:32 +0200

    Rails watch list incomplete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+require 'json'
+require 'open-uri'
+
+# Delete previous movies
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+# Seed movies from the API
+url = "http://tmdb.lewagon.com/movie/top_rated"
+response = URI.open(url).read
+movies = JSON.parse(response)["results"]
+
+movies.each do |movie|
+  Movie.create(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+end
+
+puts "#{Movie.count} movies seeded"

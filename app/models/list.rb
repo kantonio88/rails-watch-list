df5f6f478d1c6ea6956bd0ab67d4175359be3375
kantonio88 <keston.antonio.schmidtgen@gmail.com>
2023-05-26 17:04:23 +0200

    Not finish yet

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,4 +3,6 @@ class List < ApplicationRecord
 
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  
+  has_one_attached :image
 end
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module RailsWatchList
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+
   end
 end
